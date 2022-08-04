e57c1194882c7bb39e8f89b89621100f9dcbc651
U190709032 <tugbayill01@gmail.com>
2022-08-04 23:22:23 +0300

    added components

---
+++ src/components/Header.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+function Header() {
+    return (
+      <header className="d-flex justify-content-between align-items-center">
+     <nav className="container container d-flex justify-content-between align-items-center">
+       <h1>TY.</h1> 
+      <ul className="d-flex align-items-center gap-5">
+       <li>
+       <a href="">About</a></li> 
+      <li>
+      <a href="">Projects</a> 
+      </li> 
+       <li>
+       <a href="">Skills</a> 
+       </li> 
+       <li>
+       <a href="">Contact</a> 
+       </li> 
+
+      </ul> 
+       </nav> 
+      </header> 
+    )
+    }
+export default Header;
